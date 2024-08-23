732f43cdf40e2cef8bfbb723fd449ce6569ee056
Richard Gieg <richgieg@users.noreply.github.com>
2024-08-23 15:18:45 -0700

    Add HLT instruction to idle loop

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -5,6 +5,10 @@ char *const screen = (char *)0xb8000;
 const char greeting[] = "AmorFatiOS v0.0.1";
 
 void _start(void) {
+    for (int i = 0; i < 80 * 25; i++) {
+        screen[i * 2 + 1] = 0x3f;
+    }
+
     for (int i = 0; i < sizeof(greeting); i++) {
         screen[i * 2] = greeting[i];
     }
@@ -13,9 +17,7 @@ void _start(void) {
     pic_init();
     pic_unmask_irq(1);
 
-    char c = 0;
     while (1) {
-        screen[140] = c;
-        c++;
+        __asm__("hlt");
     }
 }
