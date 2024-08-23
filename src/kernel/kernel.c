16e2cd718f2ef365262861f97a7f3f4de154c15c
Richard Gieg <richgieg@users.noreply.github.com>
2024-08-23 11:51:24 -0700

    Put opening braces on same line

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,10 +1,8 @@
 char *const screen = (char *)0xb8000;
 const char greeting[] = "AmorFatiOS v0.0.1";
 
-void _start(void)
-{
-    for (int i = 0; i < sizeof(greeting); i++)
-    {
+void _start(void) {
+    for (int i = 0; i < sizeof(greeting); i++) {
         screen[i * 2] = greeting[i];
     }
     while (1);
