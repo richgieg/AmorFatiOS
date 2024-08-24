42a589a117bf5bb3dad38b1bbbd6df6bc217844a
Richard Gieg <richgieg@users.noreply.github.com>
2024-08-23 22:41:31 -0700

    Explicitly enable keyboard

--- src/kernel/keyboard.c
+++ src/kernel/keyboard.c
@@ -4,6 +4,14 @@
 #include "idt.h"
 #include "pic.h"
 
+#define STATUS_PORT 0x64
+#define COMMAND_PORT 0x64
+#define DATA_PORT 0x60
+
+#define ENABLE_KBD_PORT 0xae
+#define RESET_KBD 0xff
+#define ENABLE_SCANNING 0xf4
+
 __attribute__((naked))
 static void interrupt_service_routine(void) {
     char *p = " ";
@@ -14,6 +22,23 @@ static void interrupt_service_routine(void) {
 }
 
 void keyboard_init(void) {
+    while (inb(STATUS_PORT) & 2); // wait_input_clear
+    outb(COMMAND_PORT, ENABLE_KBD_PORT);
+
+    while (inb(STATUS_PORT) & 2); // wait_input_clear
+    outb(DATA_PORT, RESET_KBD);
+
+    // Read ACK (0xfa)
+    while ((inb(STATUS_PORT) & 1) == 0); // wait_output_full
+    inb(DATA_PORT);
+
+    // Read self-test pass (0xaa)
+    while ((inb(STATUS_PORT) & 1) == 0); // wait_output_full
+    inb(DATA_PORT);
+
+    while (inb(STATUS_PORT) & 2); // wait_input_clear
+    outb(DATA_PORT, ENABLE_SCANNING);
+
     idt_set_descriptor(IRQ1_INTERRUPT, interrupt_service_routine, 0x8e);
     pic_unmask_irq(1);
 }
