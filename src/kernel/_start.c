43e6b0ae5e909e9b1a2af52f9f30a8812b567a68
Richard Gieg <richgieg@users.noreply.github.com>
2024-08-26 13:51:33 -0700

    Clear the BSS segment

--- src/kernel/_start.c
+++ src/kernel/_start.c
@@ -1,6 +1,15 @@
 #include "kernel.h"
 
+extern char _bss_start[];
+extern char _bss_end[];
+
 void _start(void) {
+    // TODO: Make bootloader clear BSS segment instead.
+    char *bss = _bss_start;
+    while (bss < _bss_end) {
+        *bss++ = 0;
+    }
+
     kernel_init();
     kernel_idle();
 }
