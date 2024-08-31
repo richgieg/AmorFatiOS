7f937d1d4807d998f87165f49269985f65978aab
Richard Gieg <richgieg@users.noreply.github.com>
2024-08-31 14:22:50 -0700

    Add crude process switching code

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -18,32 +18,34 @@ static void process0_start() {
     }
 }
 
-static void process1_start() {
-    vga_puts("Process 1 started\n");
-    while (1) {
-        __asm__("hlt");
-    }
-}
+// static void process1_start() {
+//     vga_puts("Process 1 started\n");
+//     while (1) {
+//         __asm__("hlt");
+//     }
+// }
 
 void kernel_init(void) {
     vga_init();
-    mm_init();
-    scheduler_init();
-    idt_init();
-    pic_init();
-    timer_init();
-    ps2_init();
-    keyboard_init();
-    mouse_init();
-    pci_init();
+    // mm_init();
+    // scheduler_init();
+    // idt_init();
+    // pic_init();
+    // timer_init();
+    // ps2_init();
+    // keyboard_init();
+    // mouse_init();
+    // pci_init();
 
     // vga_puts("AmorFatiOS v0.0.1\n");
 
     // mm_show_mdump();
     // mm_show_mmap();
 
+    __asm__("cli");
+
     process_create(process0_start);
-    process_create(process1_start);
+    // process_create(process1_start);
     process_switch();
 }
 
--- src/kernel/process.c
+++ src/kernel/process.c
@@ -1,6 +1,7 @@
 #include "process.h"
 #include "types.h"
 #include "bugcheck.h"
+#include "vga.h"
 
 #define MAX_PROCESSES 8
 #define STACK_SIZE 4096
@@ -13,28 +14,11 @@ enum process_state {
     PROCESS_STATE_WAITING,
 };
 
-struct process_context {
-    u32 eax;
-    u32 ebx;
-    u32 ecx;
-    u32 edx;
-    u32 esi;
-    u32 edi;
-    u32 esp;
-    u32 ebp;
-    u16 cs;
-    u16 ds;
-    u16 es;
-    u16 fs;
-    u16 gs;
-    u32 eflags;
-};
-
 struct process {
     void (*start)();
     bool is_started;
     enum process_state state;
-    struct process_context context;
+    u32 esp;
 };
 
 static struct process processes[MAX_PROCESSES];
@@ -51,7 +35,7 @@ static int find_available_process_index() {
 }
 
 void process_create(void (*start)()) {
-    __asm__("cli"); // TODO: Replace with locking mechanism
+    // __asm__("cli"); // TODO: Replace with locking mechanism
 
     int index = find_available_process_index();
     if (index == -1) {
@@ -62,53 +46,31 @@ void process_create(void (*start)()) {
     p->start = start;
     p->is_started = false;
     p->state = PROCESS_STATE_RUNNABLE;
-    p->context.eax = 0;
-    p->context.ebx = 0;
-    p->context.ecx = 0;
-    p->context.edx = 0;
-    p->context.esi = 0;
-    p->context.edi = 0;
-    p->context.esp = STACK_AREA_BASE + (index + 1) * STACK_SIZE;
-    p->context.ebp = 0;
-    p->context.cs = 0x8;
-    p->context.ds = 0x10;
-    p->context.es = 0x10;
-    p->context.fs = 0x10;
-    p->context.gs = 0x10;
+    p->esp = STACK_AREA_BASE + (index + 1) * STACK_SIZE;
 
-    __asm__("sti"); // TODO: Replace with locking mechanism
+    // __asm__("sti"); // TODO: Replace with locking mechanism
 }
 
 void process_switch(void) {
-    __asm__("cli"); // TODO: Replace with locking mechanism
+    // __asm__("cli"); // TODO: Replace with locking mechanism
+
+    struct process *curr_proc;
+    struct process *next_proc;
 
     if (current_process_index != -1) {
-        // TODO: Store context of current process and update its state.
-        struct process *p = &processes[current_process_index];
-        p->state = PROCESS_STATE_RUNNABLE;
-        // TODO: Use inline assembly to save these registers.
-        // p->context.eax = 0;
-        // p->context.ebx = 0;
-        // p->context.ecx = 0;
-        // p->context.edx = 0;
-        // p->context.esi = 0;
-        // p->context.edi = 0;
-        // p->context.esp = 0;
-        // p->context.ebp = 0;
-        // p->context.cs = 0x8;
-        // p->context.ds = 0x10;
-        // p->context.es = 0x10;
-        // p->context.fs = 0x10;
-        // p->context.gs = 0x10;
+        curr_proc = 0;
+    } else {
+        curr_proc = &processes[current_process_index];
     }
 
     // TODO: Find next process to run, load its context, and update its state.
     int count = MAX_PROCESSES;
     int next_process_index = -1;
     int index = current_process_index + 1;
+    struct process *p;
 
     while (count--) {
-        struct process *p = &processes[index];
+        p = &processes[index];
         if (p->state == PROCESS_STATE_RUNNABLE) {
             next_process_index = index;
         }
@@ -119,5 +81,76 @@ void process_switch(void) {
         BUGCHECK("Could not find a runnable process.");
     }
 
-    __asm__("sti"); // TODO: Replace with locking mechanism
+    current_process_index = next_process_index;
+    next_proc = &processes[current_process_index];
+
+    curr_proc->state = PROCESS_STATE_RUNNABLE;
+    next_proc->state = PROCESS_STATE_RUNNING;
+
+    if (!next_proc->is_started) {
+        u32 *esp = next_proc->esp;
+
+        esp--; // eip
+        *esp = next_proc->start;
+
+        u32 eflags;
+        __asm__ volatile(
+            "pushfd;"
+            "pop %0"
+            : "=r"(eflags)
+            :
+            : "memory"
+        );
+
+        esp--; // eflags
+        *esp = eflags;
+
+        esp--; // eax
+        *esp = 0;
+
+        esp--; // ecx
+        *esp = 0;
+
+        esp--; // edx
+        *esp = 0;
+
+        esp--; // ebx
+        *esp = 0;
+
+        esp--; // esp
+        *esp = p->esp;
+
+        esp--; // ebp
+        *esp = 0;
+
+        esp--; // esi
+        *esp = 0;
+
+        esp--; // edi
+        *esp = 0;
+
+        next_proc->esp = esp;
+        next_proc->is_started = true;
+    } else {
+        __asm__(
+            "pushfd;"
+            "pushad;"
+            "mov %[old_esp], esp"
+            : [old_esp] "=m" (curr_proc->esp)
+            :
+            : "memory"
+        );
+    }
+
+    __asm__(
+        "mov esp, %[new_esp];"
+        "popad;"
+        "popfd;"
+        "ret;"
+        :
+        : [new_esp] "m" (next_proc->esp)
+        : "memory"
+    );
+
+    // __asm__("sti"); // TODO: Replace with locking mechanism
 }
