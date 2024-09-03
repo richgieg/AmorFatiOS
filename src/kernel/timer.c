99adc1586ad42d102ce78e7457a1e8bd90a18092
Richard Gieg <richgieg@users.noreply.github.com>
2024-09-03 10:20:18 -0700

    Fire timer at 100 Hz

--- src/kernel/timer.c
+++ src/kernel/timer.c
@@ -4,12 +4,22 @@
 #include "pic.h"
 #include "scheduler.h"
 
+#define BASE_FREQ_HZ 1193182
+#define DESIRED_FREQ_HZ 100
+
 static void interrupt_service_routine(void) {
     outb(PIC1_COMMAND, PIC_EOI);
     scheduler_update();
 }
 
 void timer_init(void) {
+    u16 divisor = BASE_FREQ_HZ / DESIRED_FREQ_HZ;
+    u8 low = divisor;
+    u8 high = divisor >> 8;
+    outb(0x43, 0x36);
+    outb(0x40, low);
+    outb(0x40, high);
+
     idt_set_irq_handler(0, interrupt_service_routine);
     pic_unmask_irq(0);
 }
