6c329130d7c91bf065c22dd556e41707dc59eaa7
Richard Gieg <richgieg@users.noreply.github.com>
2024-09-06 09:38:22 -0700

    Remove src directory level

--- build.sh
+++ build.sh
@@ -7,29 +7,29 @@ set -e
 mkdir -p bin
 
 # Assemble the boot sector.
-nasm -f bin -o bin/boot src/boot/boot.asm
+nasm -f bin -o bin/boot boot/boot.asm
 
 # Compile the kernel.
 gcc -masm=intel -m32 -ffreestanding -nostdlib -fno-pic -fno-pie -Wl,-no-pie -Wall -std=c11 \
-    -I./src/kernel/include -Wl,--build-id=none -Wl,-T,kernel.ld -o bin/kernel \
-    src/kernel/_start.c \
-    src/kernel/device/i82545em.c \
-    src/kernel/device/keyboard.c \
-    src/kernel/device/mouse.c \
-    src/kernel/device/pci.c \
-    src/kernel/device/pic.c \
-    src/kernel/device/ps2.c \
-    src/kernel/device/timer.c \
-    src/kernel/device/vga.c \
-    src/kernel/lib/string.c \
-    src/kernel/program/counter.c \
-    src/kernel/program/memdump.c \
-    src/kernel/program/memmap.c \
-    src/kernel/bugcheck.c \
-    src/kernel/console.c \
-    src/kernel/idt.c \
-    src/kernel/kernel.c \
-    src/kernel/process.c
+    -I./kernel/include -Wl,--build-id=none -Wl,-T,kernel.ld -o bin/kernel \
+    kernel/_start.c \
+    kernel/device/i82545em.c \
+    kernel/device/keyboard.c \
+    kernel/device/mouse.c \
+    kernel/device/pci.c \
+    kernel/device/pic.c \
+    kernel/device/ps2.c \
+    kernel/device/timer.c \
+    kernel/device/vga.c \
+    kernel/lib/string.c \
+    kernel/program/counter.c \
+    kernel/program/memdump.c \
+    kernel/program/memmap.c \
+    kernel/bugcheck.c \
+    kernel/console.c \
+    kernel/idt.c \
+    kernel/kernel.c \
+    kernel/process.c
 
 # Write boot sector and kernel to virtual floppy disk file.
 sudo dd if=/dev/zero of=bin/os.flp bs=512 count=2880
