65a0579723d783aa25527a63e8875e586ef58cb0
Richard Gieg <richgieg@users.noreply.github.com>
2024-09-09 10:05:41 -0700

    Use syscall in counter program

--- userspace/program/counter.c
+++ userspace/program/counter.c
@@ -1,16 +1,11 @@
 #include <program/counter.h>
+#include <syscall.h>
 
 void counter(void) {
     int count = 0;
+    
     while (1) {
-        // console_putdw_at(count, 0, 0);
-        __asm__(
-            "mov eax, %0;"
-            "int 0x82;"
-            :
-            :
-            "r" (count)
-        );
+        sys_console_putdw_at(count, 0, 0);
         count++;
     }
 }
