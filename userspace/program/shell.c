286683b0ba22750591bf47b1af61a693298caffa
Richard Gieg <richgieg@users.noreply.github.com>
2024-09-10 14:31:02 -0700

    Add trim function to shell

--- userspace/program/shell.c
+++ userspace/program/shell.c
@@ -1,5 +1,10 @@
 #include <program/shell.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys.h>
+
+char * trim(char *str);
 
 void shell(void) {
     char buf[2048];
@@ -9,8 +14,33 @@ void shell(void) {
     while (true) {
         puts("$> ");
         gets(buf);
+
+        char *trimmed = trim(buf);
         puts("You wrote: ");
-        puts(buf);
+        puts(trimmed);
+        puts("\n");
+        puts("Trimmed length = ");
+        size_t len = strlen(trimmed);
+        sys_console_putdw(len);
         puts("\n");
     }
+}
+
+char * trim(char *str) {
+    // Trim leading space.
+    while (isspace(*str)) str++;
+
+    // Is all space?
+    if (*str == 0) {
+        return str;
+    }
+
+    // Trim trailing space.
+    char *end = str + strlen(str) - 1;
+    while (isspace(*end)) end--;
+
+    // Write new terminator.
+    *(end + 1) = '\0';
+
+    return str;
 }
